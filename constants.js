76e0f4a2939d652b138199bd32d00d5218a35b44
Maksym Polupan <me@polupan.dev>
2023-04-08 23:27:32 +0800

    ALL RIDER!

--- constants.js
+++ constants.js
@@ -1,86 +1,17 @@
-import ideaIcon from '@jetbrains/logos/intellij-idea/intellij-idea.svg';
-import appcodeIcon from '@jetbrains/logos/appcode/appcode.svg';
-import clionIcon from '@jetbrains/logos/clion/clion.svg';
-import pycharmIcon from '@jetbrains/logos/pycharm/pycharm.svg';
-import phpstormIcon from '@jetbrains/logos/phpstorm/phpstorm.svg';
-import rubymineIcon from '@jetbrains/logos/rubymine/rubymine.svg';
-import webstormIcon from '@jetbrains/logos/webstorm/webstorm.svg';
 import riderIcon from '@jetbrains/logos/rider/rider.svg';
-import golandIcon from '@jetbrains/logos/goland/goland.svg';
 
-export const DEFAULT_LANGUAGE = 'java';
+export const DEFAULT_LANGUAGE = 'c#';
 
 export const SUPPORTED_LANGUAGES = {
-  [DEFAULT_LANGUAGE]: ['idea'],
-  kotlin: ['idea'],
-  groovy: ['idea'],
-  scala: ['idea'],
-  javascript: ['webstorm', 'phpstorm', 'idea'],
-  coffeescript: ['webstorm', 'phpstorm', 'idea'],
-  typescript: ['webstorm', 'phpstorm', 'idea'],
-  dart: ['webstorm', 'phpstorm', 'idea'],
-  go: ['goland', 'idea'],
-  css: ['webstorm', 'phpstorm', 'idea'],
-  html: ['webstorm', 'phpstorm', 'idea'],
-  python: ['pycharm', 'idea'],
-  'jupyter notebook': ['pycharm', 'idea'],
-  php: ['phpstorm', 'idea'],
-  'c#': ['rider'],
-  'f#': ['rider'],
-  'c++': ['clion'],
-  c: ['clion'],
-  ruby: ['rubymine', 'idea'],
-  rust: ['clion', 'idea'],
-  puppet: ['rubymine', 'idea'],
-  'objective-c': ['appcode'],
-  swift: ['appcode']
+  [DEFAULT_LANGUAGE]: ['rider'],
+  'c#': ['rider']
 };
 
 export const SUPPORTED_TOOLS = {
-  idea: {
-    name: 'IntelliJ IDEA',
-    tag: 'idea',
-    icon: ideaIcon
-  },
-  appcode: {
-    name: 'AppCode',
-    tag: 'appcode',
-    icon: appcodeIcon
-  },
-  clion: {
-    name: 'CLion',
-    tag: 'clion',
-    icon: clionIcon
-  },
-  pycharm: {
-    name: 'PyCharm',
-    tag: 'pycharm',
-    icon: pycharmIcon
-  },
-  phpstorm: {
-    name: 'PhpStorm',
-    tag: 'php-storm',
-    icon: phpstormIcon
-  },
-  rubymine: {
-    name: 'RubyMine',
-    tag: 'rubymine',
-    icon: rubymineIcon
-  },
-  webstorm: {
-    name: 'WebStorm',
-    tag: 'web-storm',
-    icon: webstormIcon
-  },
   rider: {
     name: 'Rider',
     tag: 'rd',
     icon: riderIcon
-  },
-  goland: {
-    name: 'GoLand',
-    tag: 'goland',
-    icon: golandIcon
   }
 };
 
